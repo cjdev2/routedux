9380a050c0ea639b42c28a13ec885cfcf6aa2f34
Max Summe <msumme@cj.com>
2017-06-19 16:16:12 -0700

    Adding package script and fixing some things

---
+++ publish.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+
+version="$1"
+
+foo="`mktemp`"
+
+sedscript=$(printf '/version/s/"[^"]*",/"%s",/' $version)
+
+sed "$sedscript" package.json > $foo;
+
+cat $foo
+grep version $foo
+
+result=y
+read -p "Correct [Y/n]? " -r result
+
+if [[ "${result/Y/y}" != 'y' ]]; then
+  exit 1;
+fi
+
+mv "$foo" package.json
+
+git add package.json
+
+git commit -v
+
+result=y
+read -p "npm publish, git tag and git push? [Y/n]? " -r result
+
+if [[ "${result/Y/y}" != 'y' ]]; then
+  exit 1;
+fi
+
+npm run buildPub
+
+git tag "v${version}"
+git push
+git push --tags
+
+
