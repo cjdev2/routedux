5e7380ffad9ee594a7fbe7e2cf7a2186e95b07a4
Max Summe <msumme@cj.com>
2020-06-29 16:40:49 -0700

    Remove empty test file

--- src/tests/provider-api.test.js
+++
@@ -1,9 +0,0 @@
-
-
-// TODO add tests that start to shape the API we would want to use
-// with something that provides a simple function to change the route and wraps the "action" interface
-// its interface should be {routeName: xyz, params: {}, query: {}} and it should create the URLs for us
-// and then trigger the provider to update somehow?
-// P
-
-//
