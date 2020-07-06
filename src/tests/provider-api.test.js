e53cdb84429870442bd51c446f63d4de711753ce
Max Summe <msumme@cj.com>
2020-07-06 09:35:00 -0700

    test: Adding some tests for provider api

---
+++ src/tests/provider-api.test.js
@@ -0,0 +1,53 @@
+import {createRouteDispatcher} from "../provider-api";
+import {createFakeWindow, createLocation} from "./test-utils";
+
+const routesConfig = [
+  ["/foo", "foo", {}],
+  ["/bar/:id", "bar", {}]
+];
+
+
+describe("routeDispatcher", () => {
+  test("receiveRoute and addRouteListener work together", () => {
+    //given
+    const _window = createFakeWindow("/foo");
+    const routeDispatcher = createRouteDispatcher(routesConfig, _window);
+    let thing = null;
+    routeDispatcher.addRouteListener((r) => { thing = r });
+    //when
+    routeDispatcher.receiveRoute({routeName: "foo"})
+    //then
+    expect(thing).toEqual({routeName: "foo"});
+
+  });
+
+  test("when receiveLocation is called, routeListener gets route not action", () => {
+    //given
+    const _window = createFakeWindow("/foo");
+    const routeDispatcher = createRouteDispatcher(routesConfig, _window);
+    let thing = null;
+    routeDispatcher.addRouteListener((r) => { thing = r });
+    //when
+    routeDispatcher.receiveLocation(createLocation("/bar/hi"))
+    //then
+    expect(thing).toEqual({routeName: "bar", id: "hi"});
+
+  });
+
+  test("currentRoute returns the correct route object", () => {
+    //given
+    const _window = createFakeWindow("/foo");
+    const routeDispatcher = createRouteDispatcher(routesConfig, _window);
+    //when
+    routeDispatcher.receiveLocation(createLocation("/bar/hi"))
+    //then
+    expect(routeDispatcher.currentRoute).toEqual({routeName: "bar", id: "hi"});
+
+  });
+});
+
+describe("RouteProvider and helpers", () => {
+  test("RouteProvider gives RouteLink correct info", () => {
+
+  });
+});
