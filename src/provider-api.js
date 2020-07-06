7c8507b147e107a2d9ed17824c9309acf6c34a69
Serra Allgood <sallgood@cj.com>
2020-07-06 12:02:00 -0700

    CJPM-14725: Add some tests
    Signed-off-by: Serra Allgood <sallgood@cj.com>

--- src/provider-api.js
+++ src/provider-api.js
@@ -1,14 +1,13 @@
-import * as R from 'ramda';
-import React, {useReducer, useEffect} from 'react';
-import PropTypes from 'prop-types';
+import * as R from "ramda";
+import React, { useReducer, useEffect } from "react";
+import PropTypes from "prop-types";
 
-import {createActionDispatcher} from "./action-router";
+import { createActionDispatcher } from "./action-router";
 
 const RouteContext = React.createContext(null);
 const ActionDispatcherContext = React.createContext(null);
 
-function RouteProvider({children, routeDispatcher, _window}) {
-
+function RouteProvider({ children, routeDispatcher, _window }) {
   const [route, updateRoute] = useReducer((state, action) => action, {});
 
   useEffect(() => {
@@ -16,68 +15,81 @@ function RouteProvider({children, routeDispatcher, _window}) {
   });
 
   useEffect(() => {
+    console.log("location received", _window.location);
     routeDispatcher.receiveLocation(_window.location);
   });
 
-  return (<ActionDispatcherContext.Provider value={routeDispatcher}>
-    <RouteContext.Provider value={route}>
-      {children}
-    </RouteContext.Provider>
-  </ActionDispatcherContext.Provider>);
+  console.log("route", route);
+
+  return (
+    <ActionDispatcherContext.Provider value={routeDispatcher}>
+      <RouteContext.Provider value={route}>{children}</RouteContext.Provider>
+    </ActionDispatcherContext.Provider>
+  );
 }
 
 RouteProvider.defaultProps = {
-  _window: window ? window : null
+  _window: window ? window : null,
 };
 
 RouteProvider.propTypes = {
   _window: PropTypes.object,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
+    PropTypes.node,
   ]),
-  routeDispatcher: PropTypes.object
+  routeDispatcher: PropTypes.object,
 };
 
 function getDisplayName(WrappedComponent) {
-  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+  return WrappedComponent.displayName || WrappedComponent.name || "Component";
 }
 
 function withRoute(Component) {
-  const routeAdded = ({children, ...restProps}) => {
-    return (<RouteContext.Consumer>
-        {route =>
-          <Component {...{...restProps, route}}>{children}</Component>
-        }
+  const routeAdded = ({ children, ...restProps }) => {
+    return (
+      <RouteContext.Consumer>
+        {(route) => {
+          console.log(route);
+          return <Component {...{ ...restProps, route }}>{children}</Component>;
+        }}
       </RouteContext.Consumer>
-    )
+    );
   };
-  routeAdded.displayName = `withRoute(${getDisplayName(Component)})`
+  routeAdded.displayName = `withRoute(${getDisplayName(Component)})`;
   routeAdded.propTypes = {
     children: PropTypes.oneOfType([
       PropTypes.arrayOf(PropTypes.node),
-      PropTypes.node
-    ])
+      PropTypes.node,
+    ]),
   };
   return routeAdded;
 }
 
-function RouteLink({routeName, params, children, ...props}) {
+function RouteLink({ routeName, params, children, ...props }) {
   const action = {
-    type: routeName, ...params
+    type: routeName,
+    ...params,
   };
-  return <ActionDispatcherContext.Consumer>{
-    dispatcher => {
-      const url = dispatcher.pathForAction(action);
-      return <a
-        onClick={(ev) => {
-          ev.preventDefault();
-          dispatcher.receiveAction(action, true);
-        }}
-        href={url} {...props}>{children}</a>
-    }
-  }
-  </ActionDispatcherContext.Consumer>;
+  return (
+    <ActionDispatcherContext.Consumer>
+      {(dispatcher) => {
+        const url = dispatcher.pathForAction(action);
+        return (
+          <a
+            onClick={(ev) => {
+              ev.preventDefault();
+              dispatcher.receiveRoute({ routeName, ...params }, true);
+            }}
+            href={url}
+            {...props}
+          >
+            {children}
+          </a>
+        );
+      }}
+    </ActionDispatcherContext.Consumer>
+  );
 }
 
 RouteLink.propTypes = {
@@ -85,31 +97,33 @@ RouteLink.propTypes = {
   params: PropTypes.object,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
+    PropTypes.node,
   ]),
 };
 
 function routeToAction(route) {
-  return R.omit(['routeName'], R.assoc('type', route.routeName, route))
+  return R.omit(["routeName"], R.assoc("type", route.routeName, route));
 }
 function actionToRoute(action) {
-  return R.omit(['type'], R.assoc('routeName', action.type, action))
+  return R.omit(["type"], R.assoc("routeName", action.type, action));
 }
 
 function createRouteDispatcher(routesConfig, _window = window) {
   const actionDispatcher = createActionDispatcher(routesConfig, _window);
 
-  actionDispatcher.receiveRoute = (route) => actionDispatcher.receiveAction(routeToAction(route), true);
-  actionDispatcher.addRouteListener = (cb) => actionDispatcher.addActionListener((action) => cb(actionToRoute(action)));
+  actionDispatcher.receiveRoute = (route) =>
+    actionDispatcher.receiveAction(routeToAction(route), true);
+  actionDispatcher.addRouteListener = (cb) =>
+    actionDispatcher.addActionListener((action) => cb(actionToRoute(action)));
 
   Object.defineProperty(actionDispatcher, "currentRoute", {
     enumerable: true,
-    get: function() {
-      return actionToRoute(this.currentAction)
-    }
+    get: function () {
+      return actionToRoute(this.currentAction);
+    },
   });
 
   return actionDispatcher;
 }
 
-export {RouteProvider, withRoute, RouteLink, createRouteDispatcher};
+export { RouteProvider, withRoute, RouteLink, createRouteDispatcher };
--- src/tests/provider-api.test.js
+++ src/tests/provider-api.test.js
@@ -1,24 +1,36 @@
-import {createRouteDispatcher} from "../provider-api";
-import {createFakeWindow, createLocation} from "./test-utils";
+import React from "react";
+import ReactDOM from "react-dom";
+import Adapter from "enzyme-adapter-react-16";
+import { act } from "react-dom/test-utils";
+
+import {
+  createRouteDispatcher,
+  RouteProvider,
+  RouteLink,
+  withRoute,
+} from "../provider-api";
+import { createFakeWindow, createLocation } from "./test-utils";
+import Enzyme, { mount } from "enzyme";
 
 const routesConfig = [
   ["/foo", "foo", {}],
-  ["/bar/:id", "bar", {}]
+  ["/bar/:id", "bar", {}],
 ];
 
-
+Enzyme.configure({ adapter: new Adapter() });
 describe("routeDispatcher", () => {
   test("receiveRoute and addRouteListener work together", () => {
     //given
     const _window = createFakeWindow("/foo");
     const routeDispatcher = createRouteDispatcher(routesConfig, _window);
     let thing = null;
-    routeDispatcher.addRouteListener((r) => { thing = r });
+    routeDispatcher.addRouteListener((r) => {
+      thing = r;
+    });
     //when
-    routeDispatcher.receiveRoute({routeName: "foo"})
+    routeDispatcher.receiveRoute({ routeName: "foo" });
     //then
-    expect(thing).toEqual({routeName: "foo"});
-
+    expect(thing).toEqual({ routeName: "foo" });
   });
 
   test("when receiveLocation is called, routeListener gets route not action", () => {
@@ -26,12 +38,13 @@ describe("routeDispatcher", () => {
     const _window = createFakeWindow("/foo");
     const routeDispatcher = createRouteDispatcher(routesConfig, _window);
     let thing = null;
-    routeDispatcher.addRouteListener((r) => { thing = r });
+    routeDispatcher.addRouteListener((r) => {
+      thing = r;
+    });
     //when
-    routeDispatcher.receiveLocation(createLocation("/bar/hi"))
+    routeDispatcher.receiveLocation(createLocation("/bar/hi"));
     //then
-    expect(thing).toEqual({routeName: "bar", id: "hi"});
-
+    expect(thing).toEqual({ routeName: "bar", id: "hi" });
   });
 
   test("currentRoute returns the correct route object", () => {
@@ -39,15 +52,91 @@ describe("routeDispatcher", () => {
     const _window = createFakeWindow("/foo");
     const routeDispatcher = createRouteDispatcher(routesConfig, _window);
     //when
-    routeDispatcher.receiveLocation(createLocation("/bar/hi"))
+    routeDispatcher.receiveLocation(createLocation("/bar/hi"));
     //then
-    expect(routeDispatcher.currentRoute).toEqual({routeName: "bar", id: "hi"});
-
+    expect(routeDispatcher.currentRoute).toEqual({
+      routeName: "bar",
+      id: "hi",
+    });
   });
 });
 
 describe("RouteProvider and helpers", () => {
   test("RouteProvider gives RouteLink correct info", () => {
+    // given
+    const _window = createFakeWindow("/foo");
+    const routeDispatcher = createRouteDispatcher(routesConfig, _window);
+    const wrapper = mount(
+      <RouteProvider routeDispatcher={routeDispatcher}>
+        <RouteLink routeName="bar" params={{ id: "foo" }} />
+      </RouteProvider>
+    );
+    // when
+    const href = wrapper.find("RouteLink").find("a").prop("href");
+    // then
+    expect(href).toEqual("/bar/foo");
+  });
 
+  test("RouteLink calls dispatcher.receiveRoute on click", () => {
+    // given
+    const _window = createFakeWindow("/foo");
+    const routeDispatcher = createRouteDispatcher(routesConfig, _window);
+    let received = null;
+    routeDispatcher.receiveRoute = (route) => {
+      received = route;
+    };
+    const wrapper = mount(
+      <RouteProvider routeDispatcher={routeDispatcher}>
+        <RouteLink routeName="bar" params={{ id: "foo" }} />
+      </RouteProvider>
+    );
+    // when
+    wrapper.find("a").simulate("click");
+    // then
+    expect(received).toEqual({ routeName: "bar", id: "foo" });
+  });
+
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+  });
+
+  test("withRoute", async () => {
+    // given
+    const Dummy = ({ route, children }) => {
+      return (
+        <div>
+          <span className="route">{JSON.stringify(route)}</span>
+          <span className="children">{children}</span>
+        </div>
+      );
+    };
+    const DummyWithRoute = withRoute(Dummy);
+    const _window = createFakeWindow("/foo");
+    const routeDispatcher = createRouteDispatcher(routesConfig, _window);
+
+    let wrapper;
+    await act(async () => {
+      ReactDOM.render(
+        <RouteProvider routeDispatcher={routeDispatcher}>
+          <DummyWithRoute>children text</DummyWithRoute>
+        </RouteProvider>,
+        container
+      );
+    });
+    // when
+
+    const routeText = container.querySelector(".route").textContext;
+    const childrenText = container.querySelector(".children").textContext;
+    // then
+    expect(routeText).toEqual(JSON.stringify({ routeName: "foo" }));
+    expect(childrenText).toEqual("children text");
   });
 });
